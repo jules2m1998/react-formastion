aad004cff85201e1b4b4f360439c0522a46ab31e
jules2m1998 <mevaajules9@gmail.com>
2022-06-09 16:37:27 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,147 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.checkSquare = this.checkSquare.bind(this);
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  checkSquare() {
+    const { tooglePlayer, i, j } = this.props;
+    tooglePlayer(i, j);
+  }
+
+  render() {
+    return (
+      <button className="square" onClick={this.checkSquare}>
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {
+    const { currentPlayer, history, tooglePlayer } = this.props;
+
+    return (
+      <div>
+        {history.map((v, k) => (
+          <div className="board-row" key={k}>
+            {v.map((value, j) => (
+              <Square
+                key={j}
+                i={k}
+                j={j}
+                value={value}
+                tooglePlayer={tooglePlayer}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPlayer: "O",
+      history: [Array(3).fill(null), Array(3).fill(null), Array(3).fill(null)],
+      index: 1,
+      logs: [[Array(3).fill(null), Array(3).fill(null), Array(3).fill(null)]],
+      win: false,
+      current: {
+        player:'O',
+        x:null,
+        y:null
+      },
+      player_logs: [
+        {
+          player:'O',
+          x:null,
+          y:null,
+          win:false
+        }
+      ]
+    };
+    this.tooglePlayer = this.tooglePlayer.bind(this);
+  }
+
+  tooglePlayer(i, j) {
+    if (!this.state.history[i][j] && !this.state.win) {
+
+      this.setState(({ history, current, logs,player_logs }) => {
+        const history_ = JSON.parse(JSON.stringify([...history]));
+        history_[i][j] = current.player;
+        return {
+          current: {...current, player: current.player === "O" ? "X" : "O", x:i,y:j},
+          history: [...history_],
+          logs: [...logs, history_],
+          win: this.checkWin(history_,j),
+          player_logs: [...player_logs, JSON.parse(JSON.stringify({x:i,y:j,player:current.player === "O" ? "X" : "O",win:this.checkWin(history_,j)}))]
+        }
+      });
+    }
+  }
+
+  checkWin(history,j){
+    const diago_1 = history.map((value,k) => (value.filter((v,l) => l+k===2)[0]))
+    const diago_2 = history.map((value,k) => (value.filter((v,l) => l===k)[0]))
+    const verty = history.map((value,k) => (value.filter((v,l) => l===j)[0]))
+    const identity = history.map(v => v.filter(h => h===this.state.currentPlayer))
+    identity.push(diago_1.filter(h => h===this.state.currentPlayer))
+    identity.push(diago_2.filter(h => h===this.state.currentPlayer))
+    identity.push(verty.filter(h => h===this.state.currentPlayer))
+    
+    return !!identity.filter(v => v.length===3).length
+  }
+
+  jumpTo(k) {
+    this.setState(({logs,player_logs,history}) => {
+      const current_log = [...logs[k]];
+      const current_p_log = {...player_logs[k]}
+      return {
+        history: current_log,
+        win: current_p_log.win,
+        current: current_p_log
+      };
+    });
+  }
+
+  render() {
+    const {current} = this.state
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board {...this.state} tooglePlayer={this.tooglePlayer} />
+        </div>{" "}
+        <div className="game-info">
+          { this.state.win ? `Winner: ${current.player === "O" ? "X" : "O"}` :  <div> Next player: {current.player} </div> }
+          <ol>
+            {this.state.logs.map((v, k) => (
+              <li key={k}>
+                <button onClick={() => this.jumpTo(k)}>
+                  {k === 0 ? "Go to game start" : "Go to move #" + k}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </div>{" "}
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
